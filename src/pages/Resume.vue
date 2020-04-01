ab27d22d43819230e8aef6c997b0231e934138a6
sevensidedmarble (Andrew Stewart) <andrewianstewart@gmail.com>
2020-04-01 15:55:13 -0400

    Update portfolio

--- src/pages/Resume.vue
+++ src/pages/Resume.vue
@@ -3,18 +3,18 @@
     <h1>Resume</h1>
 
     <h3>Backend</h3>
-    <p>I'm a full stack developer with mutliple years of experience in Ruby on Rails, in the health care industry.</p>
+    <p>I'm a remote full stack developer with multiple years of experience in Ruby on Rails, in the health care industry.</p>
     <p>I've worked with multiple large hospitals in the Michigan area integrating their EMR systems with other products, and have extensive experience with HL7 and related technologies.</p>
-    <p>In addition to backend work in Ruby on Rails, I'm familiar with a number of other web application frameworks like Django and Sinatra. I'm very familiar with SQL.</p>
+    <p>In addition to backend work in Ruby on Rails, I've worked with other Python and Ruby frameworks like Django and Sinatra. I've also become quite skilled with PostgreSQL.</p>
 
     <h3>Frontend</h3>
-    <p>I've worked extensively with both Polymer and VueJS (which this blog is made with) on the front end, and done a lot of Javascript work.</p>
+    <p>I've worked extensively with both VueJS (which this blog is made with), Polymer, and React.</p>
 
-    <h3>Devops</h3>
-    <p>Both at work and on my personal projects I use Docker and Kuberentes for literally everything, so I'm pretty familiar with both.</p>
+    <h3>DevOps</h3>
+    <p>Both at work and on my personal projects I use Docker and Kubernetes for literally everything, so I'm pretty skilled with both.</p>
 
     <h3>Other</h3>
-    <p>Like a lot of programmers, I'm pretty interested in stuff like game development and making music. Anything creative.</p>
+    <p>Like a lot of programmers, I'm pretty interested in stuff like game development and making music.</p>
 
     <h3>Cool Projects</h3>
     <ul>
@@ -29,7 +29,7 @@
       <a href="https://stackoverflow.com/users/8197386/sevensidedmarble" target="_blank" rel="noopener">Stack Overflow</a>
     </p>
 
-    <p>More detailed version of the above available upon request.</p>
+    <p>This is just my portfolio site, if you'd like a .pdf copy of my resume please let me know.</p>
   </Layout>
 </template>
 
