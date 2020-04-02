e65129056b6bebfa938413d2e8170b8e692345cc
sevensidedmarble (Andrew Stewart) <andrewianstewart@gmail.com>
2020-04-02 16:58:48 -0400

    Add new image wrapped in figure component

---
+++ src/components/FigImage.vue
@@ -0,0 +1,32 @@
+<!-- Wraps g-image components in the figure tag, among other things. -->
+<template>
+  <figure>
+    <g-image ref="img" :src="src" :alt="caption" data-zoomable @click="zoom" />
+    <figcaption>
+      {{ caption }}
+    </figcaption>
+  </figure>
+</template>
+
+<script>
+import mediumZoom from 'medium-zoom'
+
+export default {
+  name: 'FigImage',
+  props: {
+    src: String,
+    caption: String
+  },
+  methods: {
+    zoom () {
+      this.zoom.open()
+    }
+  },
+  mounted () {
+    this.zoom = mediumZoom(this.$refs.img)
+  },
+};
+</script>
+
+<style scoped>
+</style>
