21308db79c29c3dfb03d408aea08d8f49bb7a929
sevensidedmarble (Andrew Stewart) <andrewianstewart@gmail.com>
2020-04-02 16:57:48 -0400

    Comment out timeToRead for now

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -6,7 +6,8 @@
     </g-link>
     <b class="date" v-html="post.date" />
     <p class="description" v-html="post.description" />
-    <b> {{ post.timeToRead }} min read </b> &nbsp
+    <!-- TODO: I'd like to get this back somehow. -->
+    <!-- <b> {{ post.timeToRead }} min read </b> &#38;nbsp -->
     <g-link :to="post.path" class="read">Read More...</g-link>
   </div>
 </template>
