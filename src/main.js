99f700566ec8f80bf20625d584ce1b6a79fdb780
sevensidedmarble (Andrew Stewart) <andrewianstewart@gmail.com>
2020-04-02 16:58:09 -0400

    Lint the main.js file

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import DefaultLayout from "~/layouts/Default.vue";
-import "~/assets/tufte.css";
-import VueGtag from "vue-gtag";
+import DefaultLayout from "~/layouts/Default.vue"
+import "~/assets/tufte.css"
+import VueGtag from "vue-gtag"
 
-export default function(Vue, { router, head, isClient }) {
+export default function (Vue, {router, head, isClient}) {
   // Set default layout as a global component
-  Vue.component("Layout", DefaultLayout);
+  Vue.component("Layout", DefaultLayout)
 
   // Google Analytics
   Vue.use(VueGtag, {
-    config: { id: "UA-157959256-1" }
-  });
+    config: {id: "UA-157959256-1"}
+  })
 }
